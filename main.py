0f87532e096c7505821262dde1bdb4d14fbdc5f5
becklin-haston <bwhaston@gmail.com>
2021-10-29 15:58:13 -0700

    now downloading files from bucket, creating download locationdir for meteor data files (meteor_files)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import boto3
 import pandas as pd
+import os
+
+meteor_data_file_dir = "meteor_files" # TODO: move this to config.json
+
+if not os.path.exists(meteor_data_file_dir):
+    os.makedirs(meteor_data_file_dir)
 
 s3 = boto3.resource('s3')
 
@@ -8,4 +14,6 @@ s3_bucket_name = "majorly-meteoric" # TODO: move this to config.json
 s3_bucket = s3.Bucket(s3_bucket_name)
 
 for obj in s3_bucket.objects.all():
-    print(obj)
+    meteor_data_filename = obj.key
+    with open(os.path.join(meteor_data_file_dir, meteor_data_filename), "wb") as meteor_data_file:
+        s3_bucket.download_fileobj(meteor_data_filename, meteor_data_file)
