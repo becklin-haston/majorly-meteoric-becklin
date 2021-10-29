e21e310b526d80dac8fa2c11751a53a3dab328a0
becklin-haston <bwhaston@gmail.com>
2021-10-29 16:09:12 -0700

    moved download logic to proper function

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import boto3
 import pandas as pd
 import os
 
-meteor_data_file_dir = "meteor_files" # TODO: move this to config.json
 
-if not os.path.exists(meteor_data_file_dir):
-    os.makedirs(meteor_data_file_dir)
+def get_meteor_files_from_s3(s3_bucket, meteor_data_file_download_location):
 
-s3 = boto3.resource('s3')
+    for obj in s3_bucket.objects.all():
+        meteor_data_filename = obj.key
+        with open(os.path.join(meteor_data_file_download_location, meteor_data_filename), "wb") as meteor_data_file:
+            s3_bucket.download_fileobj(meteor_data_filename, meteor_data_file)
 
-s3_bucket_name = "majorly-meteoric" # TODO: move this to config.json
 
-s3_bucket = s3.Bucket(s3_bucket_name)
+if __name__ == "__main__":
 
-for obj in s3_bucket.objects.all():
-    meteor_data_filename = obj.key
-    with open(os.path.join(meteor_data_file_dir, meteor_data_filename), "wb") as meteor_data_file:
-        s3_bucket.download_fileobj(meteor_data_filename, meteor_data_file)
+    meteor_data_file_dir = "meteor_files"  # TODO: move this to config.json
+
+    s3_bucket_name = "majorly-meteoric"  # TODO: move this to config.json
+    s3 = boto3.resource('s3')
+    s3_bucket = s3.Bucket(s3_bucket_name)
+
+    if not os.path.exists(meteor_data_file_dir):
+        os.makedirs(meteor_data_file_dir)
+
+    get_meteor_files_from_s3(s3_bucket, meteor_data_file_dir)
